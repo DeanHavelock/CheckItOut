601da63acbf83a39966d1edf2cb9e0f28684ea52
Dean Havelock <dean.havelock@checkout.com>
2021-03-13 19:11:58 +0000

    api gets value from domain object

---
+++ src/CheckItOut.Application/QueryHandlers/GetPaymentService.cs
@@ -0,0 +1,15 @@
+﻿using CheckItOut.Domain;
+
+namespace CheckItOut.Application.QueryHandlers
+{
+    public class GetPaymentService
+    {
+        public string GetPayment(string id)
+        {
+            //var aggregateRoot = repository.getById(id);
+
+            var aggregateRoot = new SomeAggregateRoot(id);
+            return aggregateRoot.Id;
+        }
+    }
+}
---
+++ src/CheckItOut.Domain/SomeAggregateRoot.cs
@@ -0,0 +1,13 @@
+﻿namespace CheckItOut.Domain
+{
+    public class SomeAggregateRoot
+    {
+        public SomeAggregateRoot(string id)
+        {
+            Id = id;
+        }
+
+        public string Id { get; set; }
+        public string SomeValue { get; set; }
+    }
+}
